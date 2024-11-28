fc673f76ee61aaa738bdb8fdb33d9594dc787bec
Bruno Moreno <brunobatistacarriaomoreno@gmail.com>
2024-11-28 16:10:53 -0300

    Autorização com 'roles'

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,14 +2,17 @@
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from 'src/jwt.strategy/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/role.guard';
+import { Roles } from 'src/auth/roles.decorator'
 
 @Controller('auth')
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
     @Post('Register')
-    async register(@Body() body: { username: string; password: string}) {
-        return this.authService.register(body.username, body.password);
+    async register(@Body() body: { username: string; password: string; role: string}) {
+        return this.authService.register(body.username, body.password, body.role);
     }
 
     @Post('login')
@@ -25,8 +28,14 @@ export class AuthController {
     }
 
     @Get('protected')
-    @UseGuards(JwtAuthGuard)
+    @UseGuards(RolesGuard)
+    @Roles('admin')
     getProtectedRoute() {
-        return { message: 'Voce acessou uma rota protegida' };
+        return { message: 'Voce acessou uma rota protegida apenas para admins!' };
+    }
+    @Get('user-data')
+    @Roles('user', 'admin')
+    getUserData() {
+        return { message: 'Voce acessou uma rota protegida para usuários e administradores!'}
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,14 +3,21 @@ import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 
+interface User {
+    id: string;
+    username: string;
+    password: string;
+    role: string;
+}
+
 @Injectable()
 export class AuthService {
 
-    private users = [];
+    private users: User[] = [];
 
     constructor(private readonly jwtService: JwtService) {}
 
-    async validateUser(username: string, password: string,): Promise<any> {
+    async validateUser(username: string, password: string,): Promise<User | null> {
     const user = this.users.find(user => user.username === username);
     if (!user) {
         return null;
@@ -24,21 +31,21 @@ export class AuthService {
     return user;
 }
 
-async generateToken(user: { id: string; username: string}) {
-    const payload = { sub: user.id, username: user.username };
+async generateToken(user: User) {
+    const payload = { sub: user.id, username: user.username, role: user.role };
     return {
         access_token: this.jwtService.sign(payload),
     };
   }
 
-  async register(username: string, password: string) {
+  async register(username: string, password: string, role: string) {
     const userExists = this.users.find(user => user.username === username);
     if(userExists) {
         throw new Error('Usuário ja cadastrado!');
     }
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const newUser = { id: Date.now().toString(), username, password: hashedPassword};
+    const newUser = { id: Date.now().toString(), username, password: hashedPassword, role};
     this.users.push(newUser);
     return { message: 'Usuário registrado com sucesso!', user: newUser };
   }
